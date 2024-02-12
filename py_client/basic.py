d016558c07d3e20d04549d2afe024c77b98cdc99
DerickMghanga <derickmghanga3329@gmail.com>
2024-02-12 15:37:24 +0300

    basic reequest

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,3 +1,7 @@
 import requests
 
-endpoint = ""
+# endpoint = "https://httpbin.org/status/200/"
+endpoint = "https://httpbin.org/anything"
+
+get_response = requests.get(endpoint)  #API method
+print(get_response.text)  #print raw text response
