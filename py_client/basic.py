88d96eb426414abf3550ba50062a4a6a7c29b850
DerickMghanga <derickmghanga3329@gmail.com>
2024-02-13 14:17:36 +0300

    api app started

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,7 +1,11 @@
 import requests
 
 # endpoint = "https://httpbin.org/status/200/"
-endpoint = "https://httpbin.org/anything"
+#endpoint = "https://httpbin.org/anything"
+endpoint = " http://127.0.0.1:8000/"
 
-get_response = requests.get(endpoint)  #API method
-print(get_response.text)  #print raw text response
+get_response = requests.get(endpoint, json={"query": "Hello World!"})  #API method
+
+print(get_response.text)
+print(get_response.status_code)
+#print(get_response.json())  #print raw text response
