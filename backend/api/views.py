f3ca4ebc50a09d360513beffe85ca6a542620c8a
DerickMghanga <derickmghanga3329@gmail.com>
2024-02-13 15:53:06 +0300

    JSON parses

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
 
-# Create your views here.
+
+def api_home(request, *args, **kwargs):
+    request.body
+    body = request.body # byte string of JSON data
+
+    data = {}
+    try:
+        data = json.loads(body)  # parses a JSON string and returns a dict(python object)
+    except:
+        pass
+
+    print(data)
+    return JsonResponse(data)
