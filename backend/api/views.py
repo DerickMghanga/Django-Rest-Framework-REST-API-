ab357ba9a354d6c672bf9c06c8310897f67cff16
DerickMghanga <derickmghanga3329@gmail.com>
2024-02-14 09:46:59 +0300

    returned headers

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,14 +3,17 @@ from django.http import JsonResponse
 
 
 def api_home(request, *args, **kwargs):
-    request.body
+    #request.body
     body = request.body # byte string of JSON data
-
     data = {}
     try:
-        data = json.loads(body)  # parses a JSON string and returns a dict(python object)
+        data = json.loads(body)  # parse a JSON string and returns a dict(python object)
     except:
         pass
-
     print(data)
+    data['headers'] = dict(request.headers)
+    #print(dict(request.headers))
+    #json.dumps(dict(request.headers)) # change the headers(dict) to json string
+    data['content_type'] = request.content_type
+
     return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -6,6 +6,6 @@ endpoint = " http://127.0.0.1:8000/api/"
 
 get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello World!"}) #API method
 
-# print(get_response.text)
+print(get_response.headers)
 # print(get_response.status_code)
 # print(get_response.json())  #print raw text response
