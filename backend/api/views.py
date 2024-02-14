69c0b07b7f87cc95f786831ad26c3e5221f3630e
DerickMghanga <derickmghanga3329@gmail.com>
2024-02-14 12:42:55 +0300

    django model to dict

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,6 @@
 import json
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from django.forms.models import model_to_dict  #converts models to dict
 
 from products.models import Product
 
@@ -8,9 +9,10 @@ def api_home(request, *args, **kwargs):
     data = {}
 
     if model_data:  #if the model exists.(sometimes it may not exist)
-        data["id"] = model_data.id   # id field comes by default in models creation
-        data["title"] = model_data.title
-        data["content"] = model_data.content
-        data["price"] = model_data.price
-
+        data = model_to_dict(model_data, fields=["id", "title", "price"])
     return JsonResponse(data)
+    #     json_data_str = json.dumps(data)
+    # return HttpResponse(json_data_str, headers={"content-type": "application/json"})
+
+    #JsonResponse accepts 'dict' data types only
+    #HttpResponse accepts 'str' data types only
