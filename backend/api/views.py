1efa3b9dcce208c2f924416251dc3638450e278e
DerickMghanga <derickmghanga3329@gmail.com>
2024-02-14 14:30:57 +0300

    Rest Framework View and Response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,16 +1,25 @@
-import json
-from django.http import JsonResponse, HttpResponse
+#import json
+#from django.http import JsonResponse, HttpResponse
 from django.forms.models import model_to_dict  #converts models to dict
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from products.models import Product
 
+@api_view(["GET", "POST"])
 def api_home(request, *args, **kwargs):
+    """
+    Django Rest Framework API VIEW
+    """
     model_data = Product.objects.all().order_by("?").first()  #gets a random query and selects the first item
     data = {}
 
     if model_data:  #if the model exists.(sometimes it may not exist)
         data = model_to_dict(model_data, fields=["id", "title", "price"])
-    return JsonResponse(data)
+    return Response(data)
+
+
     #     json_data_str = json.dumps(data)
     # return HttpResponse(json_data_str, headers={"content-type": "application/json"})
 
--- py_client/basic.py
+++ py_client/basic.py
@@ -6,6 +6,6 @@ endpoint = " http://127.0.0.1:8000/api/"
 
 get_response = requests.get(endpoint, json={"product_id": 123}) #API method
 
-print(get_response.headers)
+#print(get_response.headers)
 # print(get_response.status_code)
-# print(get_response.json())  #print raw text response
+print(get_response.json())  #print raw text response
