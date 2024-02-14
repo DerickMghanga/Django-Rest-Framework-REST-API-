69ee9c44c8391cc74f53d01274344d5b1cab7180
DerickMghanga <derickmghanga3329@gmail.com>
2024-02-14 10:19:55 +0300

    request headers

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 
 def api_home(request, *args, **kwargs):
     #request.body
+    print(request.GET)
     body = request.body # byte string of JSON data
     data = {}
     try:
@@ -15,5 +16,7 @@ def api_home(request, *args, **kwargs):
     #print(dict(request.headers))
     #json.dumps(dict(request.headers)) # change the headers(dict) to json string
     data['content_type'] = request.content_type
+    data['params'] = dict(request.GET)
+    
 
     return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,8 +4,7 @@ import requests
 #endpoint = "https://httpbin.org/anything"
 endpoint = " http://127.0.0.1:8000/api/"
 
-get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello World!"}) #API method
+get_response = requests.get(endpoint, json={"query": "Hello World!"}) #API method
 
-print(get_response.headers)
 # print(get_response.status_code)
-# print(get_response.json())  #print raw text response
+print(get_response.json())  #print raw text response
