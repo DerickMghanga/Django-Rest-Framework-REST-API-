604180a38f75241f43c7bf2bbd2a616958d4fd42
DerickMghanga <derickmghanga3329@gmail.com>
2024-02-21 11:42:53 +0300

    product ListCreateApi view

--- backend/products/views.py
+++ backend/products/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
 
-class ProductCreateAPIView(generics.CreateAPIView):
+class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
@@ -16,7 +16,7 @@ class ProductCreateAPIView(generics.CreateAPIView):
             content = title
         serializer.save(content=content)
 
-product_create_view = ProductCreateAPIView.as_view()
+product_list_create_view = ProductListCreateAPIView.as_view()
 
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
@@ -25,3 +25,14 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     # lookup_field = 'pk' (primary key)
 
 product_detail_view = ProductDetailAPIView.as_view()
+
+
+class ProductListAPIView(generics.ListCreateAPIView):
+    '''
+    Not gonna use this method
+    '''
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # lookup_field = 'pk' (primary key)
+
+product_list_view = ProductListAPIView.as_view()
