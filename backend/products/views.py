555c1cb7375e8bd7752d272219b4808c0d22589a
DerickMghanga <derickmghanga3329@gmail.com>
2024-02-25 23:27:21 +0300

    session authentication and permissions

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, mixins
+from rest_framework import generics, mixins,permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -13,6 +13,7 @@ from .serializers import ProductSerializer
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def perform_create(self, serializer):
         #serializer.save(user=self.request.user)
