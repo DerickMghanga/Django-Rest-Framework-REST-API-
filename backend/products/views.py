67300e6f62894b96369845d217b54ffdffb04d86
DerickMghanga <derickmghanga3329@gmail.com>
2024-05-06 14:11:46 +0300

    rest framework settings.py update

--- backend/products/views.py
+++ backend/products/views.py
@@ -15,7 +15,6 @@ from api.authentication import TokenAuthentication
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    authentication_classes = [authentication.SessionAuthentication, TokenAuthentication]
     permission_classes = [permissions.IsAdminUser, isStaffEditorPermission] # custom permission. Order matters
 
     def perform_create(self, serializer):
